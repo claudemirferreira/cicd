8b63cfe92d546310ee0c2a89d92fd8ee7deb1fd7
Claudemir <claudemirramosferreira@gmail.com>
2024-05-11 14:05:14 -0300

    commit initial

---
+++ src/main/java/com/example/dicd/DicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.dicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicdApplication.class, args);
+	}
+
+}
